cc8efa76b77d9f9c4603f1448f9eb83d212b1bff
Cesar-Augusto-M <134884115+Cesar-Augusto-M@users.noreply.github.com>
2023-05-28 18:28:48 -0300

    Projeto de calculadora java
    Projeto de calculadora java utilizado com conhecimento em aula
    afaoram urilizados:
    Vs Code, linguagem java, , metodos de POO

---
+++ ProjetoCalculadora.java/Calculadora.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            Calculator calculator = new Calculator();
+
+            while (true) {
+                System.out.println("Escolha a operação:");
+                System.out.println("1. Adição");
+                System.out.println("2. Subtração");
+                System.out.println("3. Multiplicação");
+                System.out.println("4. Divisão");
+                System.out.println("5. Potência");
+                System.out.println("6. Raiz Quadrada");
+                System.out.println("Digite 'Q' para sair.");
+
+                String opcao = scanner.nextLine();
+
+                if (opcao.equalsIgnoreCase("Q")) {
+                    System.out.println("Programa encerrado.");
+                    break;
+                }
+
+                if (!opcao.matches("[1-6]")) {
+                    System.out.println("Opção inválida. Tente novamente.");
+                    continue;
+                }
+
+                System.out.print("Digite o primeiro número: ");
+                double numeroUm = scanner.nextDouble();
+                scanner.nextLine();
+
+                if (opcao.equals("6") && numeroUm < 0) {
+                    System.out.println("Não existe raiz de número negativo.");
+                    continue;
+                }
+
+                System.out.print("Digite o segundo número: ");
+                double numeroDois = scanner.nextDouble();
+                scanner.nextLine();
+
+                switch (opcao) {
+                    case "1":
+                        System.out.println("Resultado: " + calculator.adicao(numeroUm, numeroDois));
+                        break;
+                    case "2":
+                        System.out.println("Resultado: " + calculator.subtracao(numeroUm, numeroDois));
+                        break;
+                    case "3":
+                        System.out.println("Resultado: " + calculator.multiplicacao(numeroUm, numeroDois));
+                        break;
+                    case "4":
+                        if (numeroDois == 0) {
+                            System.out.println("Impossível realizar uma divisão por zero.");
+                        } else {
+                            System.out.println("Resultado: " + calculator.divisao(numeroUm, numeroDois));
+                        }
+                        break;
+                    case "5":
+                        System.out.println("Resultado: " + calculator.potencia(numeroUm, numeroDois));
+                        break;
+                    case "6":
+                        System.out.println("Resultado: " + calculator.raizQuadrada(numeroUm));
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
